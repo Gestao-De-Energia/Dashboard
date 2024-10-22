9ef3231fbabaa694f3ca9eda36e56bffa1b8b3ee
victorpss <victorpss@dcc.ufrj.br>
2024-10-22 00:05:54 -0300

    ajuste na indentação

--- public/javascript/scriptlog.js
+++ public/javascript/scriptlog.js
@@ -4,27 +4,23 @@ pwFields = document.querySelectorAll(".password"),
 signUp = document.querySelector(".signup-link"),
 login = document.querySelector(".login-link");
 
-pwShowHide.forEach(eyeIcon=>{
-    eyeIcon.addEventListener("click",()=>{
-        pwFields.forEach(pwField=>{
-
-            if(pwField.type==="password"){
-                pwField.type ="text";
-
-                pwShowHide.forEach(icon =>{
-                    icon.classList.replace("fa-eye-slash","fa-eye" );
-                })
-            }else{
-                pwField.type="password";
-                pwShowHide.forEach(icon =>{
-                    icon.classList.replace("fa-eye","fa-eye-slash" );
-                })
+pwShowHide.forEach((eyeIcon) => {
+    eyeIcon.addEventListener("click", () => {
+        pwFields.forEach((pwField) => {
+            if (pwField.type === "password") {
+                pwField.type = "text";
+                pwShowHide.forEach((icon) => {
+                    icon.classList.replace("fa-eye-slash", "fa-eye");
+                });
+            } else {
+                pwField.type = "password";
+                pwShowHide.forEach((icon) => {
+                    icon.classList.replace("fa-eye", "fa-eye-slash");
+                });
             }
-        })
-
-    })
-
-})
+        });
+    });
+});
 
 
 signUp.addEventListener("click",( )=>{
