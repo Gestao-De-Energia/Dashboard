6f4a903f8440d974ca14acc6feeedccd8f3cf8de
victorpss <victorpss@dcc.ufrj.br>
2025-03-09 18:57:54 -0300

    fix: text animation while signing in

--- public/pages/conta/scriptLogin.js
+++ public/pages/conta/scriptLogin.js
@@ -100,10 +100,10 @@ document.addEventListener("DOMContentLoaded", () => {
                 loginWarning.textContent = "Erro: Perfil do usuário não definido.";
             }
         } catch (error) {
-            stopLoadingAnimation();
             console.error("Erro ao autenticar:", error);
             loginWarning.style.display = "flex";
             loginWarning.textContent = "Erro no email ou senha.";
+            stopLoadingAnimation();
         }
     };
 
